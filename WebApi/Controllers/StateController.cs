92df5957a6182cbca20ad6a9238b259a8fcbabba
Roy <lxysac@foxmail.com>
2021-03-23 17:07:57 +0800

    进程状态

---
+++ WebApi/Controllers/StateController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StateController : ControllerBase
+    {
+        /// <summary>
+        /// 获取进程名称
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string Get()
+        {
+            var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            return "进程名字是："+processName;
+        }
+    }
+}
